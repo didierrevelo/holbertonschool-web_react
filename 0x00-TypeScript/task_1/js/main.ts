c55463e1f0ef193c24b26db0f967ec255b0fa77a
didierrevelo <1829@holbertonschool.com>
2022-01-31 15:40:06 -0500

    1. Let's build a Teacher interface

---
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -0,0 +1,38 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  /* eslint-disable @typescript-eslint/no-explicit-any*/
+  [key: string]: any;
+  /* eslint-enable @typescript-eslint/no-explicit-any*/
+}
+
+// const teacher1: Teacher = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   fullTimeEmployee: true,
+//   yearsOfExperience: 5,
+//   location: "New York",
+//   contract: false,
+// }
+
+// const teacher2: Teacher = {
+//   firstName: "Jane",
+//   lastName: "Doe",
+//   fullTimeEmployee: true,
+//   yearsOfExperience: 5,
+//   location: "New York",
+//   contract: false,
+// }
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  yearsOfExperience: 5,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+}
+
+console.log(teacher3);
