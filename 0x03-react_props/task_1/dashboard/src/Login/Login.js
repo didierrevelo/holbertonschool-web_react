cba11fa6e1186a694927cef02c2c7917a2e980fa
didierrevelo <1829@holbertonschool.com>
2022-02-15 10:20:39 -0500

    1. Write the tests for each component

---
+++ 0x03-react_props/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Login.css";
+import holberton_logo from "../assets/holberton_logo.jpg";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+
+function Login() {
+  return (
+    <>
+      <div className="login">
+        <p>Login to access the full dashboard</p>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" />
+        <button>OK</button>
+      </div>
+    </>
+  );
+}
+
+export default Login;
