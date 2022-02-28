28419ab974a3e0d3d59d10fe4f37a05082d9e5a6
didierrevelo <1829@holbertonschool.com>
2022-02-28 01:18:28 -0500

    1. install Aphrodite

--- 0x05-React_inline_styling/task_1/dashboard/src/HOC/WithLogging.test.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/HOC/WithLogging.test.js
@@ -2,8 +2,16 @@ import { shallow, mount } from "enzyme";
 import React from "react";
 import WithLogging from "./WithLogging";
 import Login from "../Login/Login";
+import { StyleSheetTestUtils } from "aphrodite";
 
 describe("<WithLogging />", () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
   it("calls console.log on mount and on unmount with Component when the wrapped element is pure html", () => {
     console.log = jest.fn();
 
