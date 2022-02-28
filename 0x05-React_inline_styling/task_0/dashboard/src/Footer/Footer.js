d1e25f7b000df905e36f9329593d92dab83cf2f2
didierrevelo <1829@holbertonschool.com>
2022-02-28 00:26:02 -0500

    0. Inline styling

---
+++ 0x05-React_inline_styling/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Footer;
