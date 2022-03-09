4ad5cfc0f2c04116972ecfc7bab94ace1f401176
didierrevelo <1829@holbertonschool.com>
2022-03-09 13:45:20 -0500

    all tasks

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,7 +1,7 @@
 import { fromJS } from 'immutable';
 
 function accessImmutableObject(object, array) {
-  return array.reduce((acc, key) => acc.get(key), fromJS(object));
+  return array.reduce((acc, key) => acc.getIn(key), fromJS(object));
 }
 
 export default accessImmutableObject;
