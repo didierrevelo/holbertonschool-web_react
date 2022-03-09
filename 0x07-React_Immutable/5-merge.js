f3a423331faccf1e8600fe9dd3ce1d63371f9ffe
didierrevelo <1829@holbertonschool.com>
2022-03-09 15:28:41 -0500

    all tasks

---
+++ 0x07-React_Immutable/5-merge.js
@@ -0,0 +1,15 @@
+import { List, Map } from 'immutable';
+
+export default function concatElements(page1, page2) {
+  const pageList1 = List(page1);
+  const pageList2 = List(page2);
+
+  return pageList1.concat(pageList2);
+}
+
+export default function mergeElements(page1, page2) {
+  const pageMap1 = Map(page1);
+  const pageMap2 = Map(page2);
+
+  return pageMap1.merge(pageMap2);
+}
