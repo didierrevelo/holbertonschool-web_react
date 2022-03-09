096ae37a880209305bf70f070fa9774075c36383
didierrevelo <1829@holbertonschool.com>
2022-03-09 14:44:07 -0500

    all tasks

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -10,5 +10,5 @@ export const map = Map({
 });
 
 export const map2 = map.withMutations((mapItem) => {
-  mapItem.set('2', 'Benjamin').set('4', 'Oliver');
+  mapItem.set(2, 'Benjamin').set(4, 'Oliver');
 });
